8e1768d05e2b0c02b65ec5c1366a4de744649536
Heather Guy <guyh@host425.jc.rl.ac.uk>
2023-10-10 14:19:27 +0100

    run for 20 mins

--- netcdf_main.sh
+++ netcdf_main.sh
@@ -22,6 +22,7 @@ stop_dat='202306120000'
 #python parse_surface-met.py $in_loc $netcdf_out $start_dat $stop_dat
 
 python parse_flux_estimates.py $in_loc $netcdf_out $start_dat $stop_dat 30
+python parse_flux_estimates.py $in_loc $netcdf_out $start_dat $stop_dat 20
 python parse_flux_estimates.py $in_loc $netcdf_out $start_dat $stop_dat 15
 
 
