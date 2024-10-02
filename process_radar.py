25742017d3636c7327e9d3e2bdf2cbba7b9b94d5
Heather Guy <heatherguy@users.noreply.github.com>
2024-10-02 11:33:09 +0100

    corrected radar height

--- process_radar.py
+++ process_radar.py
@@ -100,7 +100,11 @@ def main():
         radar.attrs['time_coverage_end'] = str(radar.time.max().data)
         radar.attrs['geospatial_bounds'] = "%sN, %sE, %sN, %sE"%(str(radar.latitude.max().data),str(radar.longitude.min().data),str(radar.latitude.min().data),str(radar.longitude.max().data))
         radar.attrs['platform_altitude'] = "Oden 4th deck, ~20 m a.s.l"
+
+        # Make sure the height is correct assuming a height amsl of 20m on the 4th deck
         radar.altitude.data=np.array(20.)
+        radar.height.data = radar.range.data + list(set(radar.altitude.data))[0]
+        
         radar.attrs['location_keywords'] = "Oden, Arctic Ocean, Fram Strait, atmosphere, profile, on the ship"
         radar.attrs['date_created'] = dt.datetime.strftime(dt.datetime.now(),'%d %b %Y %H:%M')
         radar.attrs['institution']="Stockholm University and the University of Leeds"
